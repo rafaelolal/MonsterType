7d73eb6e03fd00911355007513ec32d74155efdd
Sandro Imerlishvili <redgamer821@gmail.com>
2023-01-20 23:34:33 -0500

    Finished basic version of mainMenuScreen Implemented clickable buttons Finalized replay functionality

--- core/src/com/asd/monstertype/Assets.java
+++ core/src/com/asd/monstertype/Assets.java
@@ -1,11 +1,14 @@
 package com.asd.monstertype;
 
+import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.SkinLoader;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class Assets {
 
@@ -13,12 +16,15 @@ public class Assets {
 
     // textures
 
-    public static final String mainMenuBackground = "walter.png";
+    public static final String mainMenuBackground = "backgrounds/walter.png";
+    public static final String mainMenuBackground2 = "backgrounds/walterJunior.png";
+    public static final String mainMenuBackground3 = "backgrounds/saulGoodman.png";
     public static final String textureAtlas = "images.atlas";
     public static final String explosionTexture = "explosion.png";
 
     // music
 
+    public static final String mainMenuMusic = "mainMenuMusic.mp3";
     public static final String saulMusic = "saulMusic.mp3";
     public static final String saulSound = "saulSound.mp3";
     public static final String vineBoom = "vineBoom.mp3";
@@ -29,24 +35,25 @@ public class Assets {
     public static final String hectorHurt = "hectorHurt.mp3";
     public static final String mikeHurt = "mikeHurt.mp3";
     public static final String explosionSound = "explosion.mp3";
-
     public static final String bigExplosionSound = "bigExplosion.mp3";
 
-    // fonts
+    // ui skin
 
-    public static final String font1generator = "BreakingBad.otf";
-    public static final String font2generator = "EdgeFont.otf";
+    public static final AssetDescriptor<Skin> SKIN = new AssetDescriptor<Skin>("uiskin/uiskin.json", Skin.class, new SkinLoader.SkinParameter("uiskin/uiskin.atlas"));
 
     public static void load() {
 
         // textures
 
         manager.load(mainMenuBackground, Texture.class);
+        manager.load(mainMenuBackground2, Texture.class);
+        manager.load(mainMenuBackground3, Texture.class);
         manager.load(textureAtlas, TextureAtlas.class);
         manager.load(explosionTexture, Texture.class);
 
         // music
 
+        manager.load(mainMenuMusic, Music.class);
         manager.load(saulMusic, Music.class);
         manager.load(saulSound, Music.class);
         manager.load(vineBoom, Music.class);
@@ -59,11 +66,9 @@ public class Assets {
         manager.load(explosionSound, Sound.class);
         manager.load(bigExplosionSound, Sound.class);
 
-        // fonts
+        // uiSkin
 
-        //manager.load(font1generator, FreeTypeFontGenerator.class);
-        //manager.load(font2generator, FreeTypeFontGenerator.class);
-
+        manager.load(SKIN);
 
     }
 
--- core/src/com/asd/monstertype/MainMenuScreen.java
+++ core/src/com/asd/monstertype/MainMenuScreen.java
@@ -3,6 +3,7 @@ package com.asd.monstertype;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Camera;
@@ -15,16 +16,22 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.utils.viewport.*;
+import com.sun.org.apache.xpath.internal.operations.Or;
 
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Locale;
 
 
-public class MainMenuScreen implements Screen {
+public class MainMenuScreen extends ScreenAdapter {
 
     private GameClass parent;
 
@@ -32,18 +39,18 @@ public class MainMenuScreen implements Screen {
 
     private Camera camera;
     private Viewport viewport;
+    private Stage stage;
+    private Skin skin;
+    private Table mainTable;
 
     //graphics
-
     private SpriteBatch batch;
 
-    private TextureAtlas textureAtlas;
+    private Texture[] backgrounds;
 
-    private TextureRegion[] backgrounds;
+    // timing
 
-    //timing
-
-    private float[] backgroundOffsets = {0, 0, 0, 0, 0, 0, 0, 0};
+    private float[] backgroundOffsets = {0, 0, 0};
     private float backgroundMaxScrollingSpeed;
 
     // world parameters
@@ -51,28 +58,50 @@ public class MainMenuScreen implements Screen {
     private final int WORLD_WIDTH = 1280;
     private final int WORLD_HEIGHT = 720;
 
-    private boolean playWithAI = true;
+    private boolean playWithAI;
 
     // HUD
 
     BitmapFont font1;
-    BitmapFont font2;
 
-    // Font Generators
+    // font Generators
 
     private FreeTypeFontGenerator fontGenerator;
     private FreeTypeFontGenerator.FreeTypeFontParameter fontParameter;
 
-    MainMenuScreen(GameClass gameClass) {
+    // audio
+
+    private Music mainMenuMusic;
+
+    public MainMenuScreen(GameClass gameClass) {
 
         // GameClass Setup
 
         parent = gameClass;
 
-        camera = new OrthographicCamera();
-        viewport = new StretchViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+        // initialize backgrounds
+
+        backgrounds = new Texture[3];
+
+        backgrounds[0] = Assets.manager.get(Assets.mainMenuBackground, Texture.class);
+        backgrounds[1] = Assets.manager.get(Assets.mainMenuBackground2, Texture.class);
+        backgrounds[2] = Assets.manager.get(Assets.mainMenuBackground3, Texture.class);
+
+        backgroundMaxScrollingSpeed =  (float)(WORLD_HEIGHT) / 2;
+
+        // FONTS & HUD
 
         initializeFonts();
+
+        skin = Assets.manager.get(Assets.SKIN);
+
+        // audio
+
+        mainMenuMusic = Assets.manager.get(Assets.mainMenuMusic, Music.class);
+
+        mainMenuMusic.setVolume(0.7f);
+        mainMenuMusic.play();
+
 
         batch = new SpriteBatch();
 
@@ -92,9 +121,7 @@ public class MainMenuScreen implements Screen {
 
         font1 = fontGenerator.generateFont(fontParameter);
 
-        fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("EdgeFont.otf"));
-
-        font2 = fontGenerator.generateFont(fontParameter);
+        font1.getData().setScale(1.5f);
 
     }
 
@@ -105,24 +132,53 @@ public class MainMenuScreen implements Screen {
 
         detectInput(deltaTime);
 
-        batch.draw(Assets.manager.get(Assets.mainMenuBackground, Texture.class), 0, 0, WORLD_WIDTH, WORLD_HEIGHT);
+        renderBackgrounds(deltaTime);
 
-        font1.draw(batch, "Breaking Bad Gme", WORLD_WIDTH * 1/4, WORLD_HEIGHT * 1/2 + font2.getXHeight(), (float)WORLD_WIDTH * 1/2, Align.center, false);
+        font1.draw(batch, "Breaking Bad Gme", WORLD_WIDTH * 1/4, WORLD_HEIGHT * 2/3 + font1.getXHeight(), (float)WORLD_WIDTH * 1/2, Align.center, false);
+
+        stage.act();
+        stage.draw();
 
         batch.end();
+
+    }
+
+    private void renderBackgrounds(float deltaTime) {
+
+        backgroundOffsets[0] += (deltaTime * backgroundMaxScrollingSpeed * 1/2);
+
+        batch.draw(backgrounds[0], 0, -backgroundOffsets[0], WORLD_WIDTH, WORLD_HEIGHT);
+        batch.draw(backgrounds[0], 0, -backgroundOffsets[0] + WORLD_HEIGHT, WORLD_WIDTH, WORLD_HEIGHT);
+
+        if (backgroundOffsets[0] > WORLD_HEIGHT) {
+            backgroundOffsets[0] = 0;
+        }
+
+        backgroundOffsets[1] += (deltaTime * backgroundMaxScrollingSpeed * 3/2);
+
+        batch.draw(backgrounds[1], -backgroundOffsets[1], 60, WORLD_WIDTH / (5/2), WORLD_HEIGHT / (5/2));
+        batch.draw(backgrounds[1], -backgroundOffsets[1] + WORLD_WIDTH, 60, WORLD_WIDTH / (5/2), WORLD_HEIGHT / (5/2));
+
+        if (backgroundOffsets[1] > WORLD_WIDTH) {
+            backgroundOffsets[1] = 0;
+        }
+
+        backgroundOffsets[2] += (deltaTime * backgroundMaxScrollingSpeed * 3/2);
+
+        batch.draw(backgrounds[2], backgroundOffsets[2], 450, WORLD_WIDTH / 3, WORLD_HEIGHT / 3);
+        batch.draw(backgrounds[2], backgroundOffsets[2] - WORLD_WIDTH, 450, WORLD_WIDTH / 3, WORLD_HEIGHT / 3);
+
+        if (backgroundOffsets[2] > WORLD_WIDTH) {
+            backgroundOffsets[2] = 0;
+        }
+
+
 
     }
 
     protected boolean getPlayWithAI() {return playWithAI;}
 
     private void detectInput(float deltaTime) {
-
-        if (Gdx.input.isKeyPressed(Input.Keys.P)) {
-
-            parent.changeScreen(GameClass.GAME);
-            dispose();
-
-        }
 
         // escape
 
@@ -135,6 +191,53 @@ public class MainMenuScreen implements Screen {
     }
 
     @Override
+    public void show() {
+
+        camera = new OrthographicCamera();
+        viewport = new StretchViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+        stage = new Stage(viewport);
+
+        mainTable = new Table();
+        mainTable.setFillParent(true);
+
+        stage.addActor(mainTable);
+
+        mainTable.setPosition(0, -100);
+
+        addButton("Play With AI").addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent even, float x, float y)
+            {
+                playWithAI = true;
+                dispose();
+                parent.changeScreen(GameClass.GAME);
+            }
+        });
+        addButton("Play With Two Players").addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent even, float x, float y)
+            {
+                playWithAI = false;
+                dispose();
+                parent.changeScreen(GameClass.GAME);
+            }
+        });
+
+        Gdx.input.setInputProcessor(stage);
+
+    }
+
+    private TextButton addButton(String name) {
+
+        TextButton button = new TextButton(name, skin);
+
+        mainTable.add(button).width(700).height(50).padBottom(25);
+        mainTable.row();
+        return button;
+
+    }
+
+    @Override
     public void resize(int width, int height) {
 
         viewport.update(width, height, true);
@@ -143,33 +246,20 @@ public class MainMenuScreen implements Screen {
     }
 
     @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void show() {
-
-
-    }
-
-    @Override
     public void dispose() {
 
-        //batch.dispose();
+        // fonts
+
         font1.dispose();
-        font2.dispose();
-        //Assets.manager.unload(Assets.mainMenuBackground);
+
+        // audio
+
+        mainMenuMusic.dispose();
+
+        stage.dispose();
+
+        //batch.dispose();
+
 
     }
 
